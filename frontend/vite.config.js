8918ad0c6e34d3b5470f89a7c1afde5331d58798
friverokiefer <friverokiefer@gmail.com>
2024-09-16 20:28:23 -0300

    prueba deploy render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Verificar si estamos en entorno de producción
+const backendUrl = process.env.NODE_ENV === 'production'
+  ? 'https://backend-music_store_firk.onrender.com' // URL del backend en producción (Render)
+  : 'http://localhost:5000'; // URL del backend en desarrollo
+
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
@@ -10,5 +15,9 @@ export default defineConfig({
     commonjsOptions: {
       transformMixedEsModules: true, // Esto puede ayudar a manejar dependencias mixtas CJS/ESM
     },
+    outDir: 'dist', // Asegura que la carpeta de salida sea 'dist'
   },
+  define: {
+    'process.env.VITE_BACKEND_URL': JSON.stringify(backendUrl),
+  }
 })
